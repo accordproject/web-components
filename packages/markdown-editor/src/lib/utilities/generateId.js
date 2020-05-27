7acf20600e5abe4ddc6e148dd21990fb65f3f84e
Jolene Langlinais <jolenelanglinais@gmail.com>
2020-05-27 11:37:36 -0400

    feat(markdown-editor): generate id on whole header (#106)
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- packages/markdown-editor/src/lib/utilities/generateId.js
+++ packages/markdown-editor/src/lib/utilities/generateId.js
@@ -1,5 +1,8 @@
 // https://github.com/thlorenz/anchor-markdown-header/blob/87e4cca4618271e363f4af9697df0f73f23b3d3f/anchor-markdown-header.js#L20
-const generateId = element => element.children[0].text.replace(/ /g, '-')
+
+const childReducer = (children) => children.reduce((acc, cur) => `${acc}${cur.text}`, '');
+
+const generateId = element => childReducer(element.children).replace(/ /g, '-')
   // escape codes
   .replace(/%([abcdef]|\d){2,2}/ig, '')
   // single chars that are removed
