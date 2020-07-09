c076ecf145ab5fe21f989851449fd1985804cce8
irmerk <jolenelanglinais@gmail.com>
2020-07-09 11:37:45 -0400

    fix(ui-markdown-editor): remove ui to page break - I24
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- packages/ui-markdown-editor/src/lib/components/Span/HorizontalRule.js
+++ packages/ui-markdown-editor/src/lib/components/Span/HorizontalRule.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const HorizontalRuleWrapper = styled.div`
   overflow: visible; /* For IE */
@@ -8,6 +9,7 @@ const HorizontalRuleWrapper = styled.div`
   text-align: center;
   display: grid;
   grid-template-columns: auto max-content auto;
+  user-select: none;
 `;
 
 const HorizontalLine = styled.div`
@@ -18,19 +20,26 @@ const HorizontalLine = styled.div`
   height: 1px;
   background-color: #333;
   text-align: center;
+  user-select: none;
 `;
 
 const PageBreak = styled.span`
   overflow: visible; /* For IE */
   padding: 0 20px;
+  user-select: none;
 `;
 
 const HorizontalRule = (props) => (
-    <HorizontalRuleWrapper {...props}>
+    <HorizontalRuleWrapper contentEditable={false} {...props.attributes}>
       <HorizontalLine />
-      <PageBreak>PAGE BREAK</PageBreak>
+      <PageBreak contentEditable={false}>{props.children}PAGE BREAK</PageBreak>
       <HorizontalLine />
     </HorizontalRuleWrapper>
 );
+
+HorizontalRule.propTypes = {
+  attributes: PropTypes.any,
+  children: PropTypes.any,
+};
 
 export default HorizontalRule;
