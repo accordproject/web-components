8f4298537c4d826ba41555dbfb36fdcf8415ca72
unknown <shahmanan170602@gmail.com>
2020-09-17 14:48:38 +0530

    The z-index of the optionaTooltip was set to 1000 so that it would be seen over the buttons
    Signed-off-by: unknown <shahmanan170602@gmail.com>

--- packages/ui-contract-editor/src/lib/components/Optional/OptionalBoolean.js
+++ packages/ui-contract-editor/src/lib/components/Optional/OptionalBoolean.js
@@ -35,7 +35,7 @@ const OptionalBoolean = (props) => {
     ref,
     currentHover: hoveringOptional,
     className: 'optionalTooltip',
-    style: { marginTop: `-${tooltipHeight + 10}px`,zIndex:10000 },
+    style: { marginTop: `-${tooltipHeight + 10}px`,zIndex:1000 },
     caretTop: tooltipHeight - 2,
     caretLeft: 2,
   };
