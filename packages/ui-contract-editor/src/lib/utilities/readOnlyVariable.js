0005aa166df2a182ef3c91b1a01cae835efa2e2b
Dan Selman <danscode@selman.org>
2020-09-19 10:26:28 +0100

    chore(contract-editor) : protect against invalid decorators
    Signed-off-by: Dan Selman <danscode@selman.org>

--- packages/ui-contract-editor/src/lib/utilities/readOnlyVariable.js
+++ packages/ui-contract-editor/src/lib/utilities/readOnlyVariable.js
@@ -2,14 +2,22 @@ import { Node } from 'slate';
 import { Decorators } from '@accordproject/markdown-cicero';
 
 export const isReadOnlyVariable = (variableNode) => {
-  const decorators = new Decorators(variableNode.data);
-  return (decorators.getDecoratorValue('ContractEditor', 'readOnly') === true);
+  try {
+    const decorators = new Decorators(variableNode.data);
+    return (decorators.getDecoratorValue('ContractEditor', 'readOnly') === true);
+  } catch (err) {
+    return false;
+  }
 };
 
 export const getOpacity = (variableNode) => {
-  const decorators = new Decorators(variableNode.data);
-  const opacity = decorators.getDecoratorValue('ContractEditor', 'opacity');
-  return opacity !== undefined ? opacity : 1;
+  try {
+    const decorators = new Decorators(variableNode.data);
+    const opacity = decorators.getDecoratorValue('ContractEditor', 'opacity');
+    return opacity !== undefined ? opacity : 1;
+  } catch (err) {
+    return 1;
+  }
 };
 
 export const inReadOnlyVariable = (editor) => {
