7acc65ca61da8bb7f5c354fc27d340476efe2fee
Justin Langlinais <justinclanglinais@gmail.com>
2020-10-01 11:27:44 -0700

    style(FormattingToolbar): adjust box shadow to spec - I51
    Signed-off-by: Justin Langlinais <justinclanglinais@gmail.com>

--- packages/ui-markdown-editor/src/lib/FormattingToolbar/ToolbarMenu.js
+++ packages/ui-markdown-editor/src/lib/FormattingToolbar/ToolbarMenu.js
@@ -6,12 +6,13 @@ const Menu = styled.div`
   top: 0;
   width: 100%;
   background-color: #FFF;
-  padding: 15px;
+  padding: 15px 15px 5px 15px;
   z-index: 10;
   display: flex;
   align-content: space-evenly;
   justify-content: center;
   margin-bottom: 20px;
+  box-shadow: 0px 0px 15px 15px rgba(255,255,255,1);
   & > * {
     display: inline-block;
   }
