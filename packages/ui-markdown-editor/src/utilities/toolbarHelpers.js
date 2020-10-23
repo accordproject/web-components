729e52e7aee27168b539f539d0dd57c15a03c933
amanda-ariyaratne <aariyaratne@gmail.com>
2020-10-23 10:03:15 +0530

    style(ui-markdown-editor/index*hotkeys*toolbarHelpers): apply style suggestions - I102
    Signed-off-by: amanda-ariyaratne <aariyaratne@gmail.com>

--- packages/ui-markdown-editor/src/utilities/toolbarHelpers.js
+++ packages/ui-markdown-editor/src/utilities/toolbarHelpers.js
@@ -99,18 +99,19 @@ export const insertLinebreak = (editor, type) => {
 };
 
 export const insertHeadingbreak = (editor) => {
-  const text = {object: 'text', text: '' };
-  const n = { object: "block", type: 'paragraph', children: [text]};
+  const text = { object: 'text', text: '' };
+  const n = { object: "block", type: 'paragraph', children: [text] };
   Transforms.insertNodes(editor, n);
   return;
 }
 
 export const isBlockHeading = (editor) => {
   const [match] = Editor.nodes(editor, {
-    match: n => { return n.type === 'heading_one'
-                        | n.type === 'heading_two'
-                        | n.type === 'heading_three'
-                },
+    match: n => { 
+      return n.type === 'heading_one'
+        || n.type === 'heading_two'
+        || n.type === 'heading_three'
+    },
   });
   return !!match;
 };
