d760b1099f69a00a22110d5a94363da747b52f8b
Sanket Shevkar <43399522+sanketshevkar@users.noreply.github.com>
2021-02-09 04:35:28 +0530

    fix(ui-contract-editor): prevent clause drag and drop in list - I157 (#231)
    Signed-off-by: User <shevkar.sanket@gmail.com>

--- packages/ui-contract-editor/src/ContractEditor/index.js
+++ packages/ui-contract-editor/src/ContractEditor/index.js
@@ -207,9 +207,32 @@ const ContractEditor = (props) => {
     if (!clauseNodeAndPath) return true; // continue to next handler if not a clause
     const node = ReactEditor.toSlateNode(editor, event.target);
     const path = ReactEditor.findPath(editor, node);
-
     const nodes = [...Node.ancestors(editor, path, { reverse: false })];
-    // first node is root editor so second will be top level after root editor
+    //placementNodeType is used store node type and used to compare if a node is a list or not.
+    const placementNodeType = nodes.length > 1 ? nodes[1][0].type : null;
+    if(placementNodeType === "ul_list" || placementNodeType === "ol_list"){
+      //Node of type List stored in placementNode and its path in placementNodepath
+      const placementNode = nodes[1][0];
+      const placementNodePath = ReactEditor.findPath(editor, placementNode);
+      const listSize=nodes[1][0].children.length;
+      //currentNodeIndex cpatures the list-item index
+      const currentNodeIndex=nodes[2][1][1];
+      //comparison to decide to place it above or below the list
+      if(listSize/2 > currentNodeIndex){
+        if(clauseNodeAndPath[1] > placementNodePath[0]){
+          Transforms.moveNodes(editor, { at: clauseNodeAndPath[1], to: placementNodePath });
+        }else{
+          Transforms.moveNodes(editor, { at: clauseNodeAndPath[1], to: [placementNodePath[0] - 1] });
+        }
+      }else{
+        if(clauseNodeAndPath[1] > placementNodePath[0]){
+          Transforms.moveNodes(editor, { at: clauseNodeAndPath[1], to: [placementNodePath[0] + 1] });
+        }else{
+          Transforms.moveNodes(editor, { at: clauseNodeAndPath[1], to: placementNodePath });
+        }
+      }
+    }else{
+      // first node is root editor so second will be top level after root editor
     const topLevelNodeAndPath = nodes[1];
     // if no top level after editor then the node was already a top level node so use its own path
     const topLevelPath = topLevelNodeAndPath ? topLevelNodeAndPath[1] : path;
@@ -237,6 +260,7 @@ const ContractEditor = (props) => {
     Transforms.collapse(editor, { edge });
     Transforms.removeNodes(editor, { at: sourceRange.anchor.path, match: n => n.type === CLAUSE });
     Transforms.insertNodes(editor, clauseNodeAndPath[0]);
+    }
     return false;
   };
 
